51e3471d3a942c9c49079371d81a87958e4ffbdc
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-25 21:57:08 +0530

    fibonacci series generation
    Fix loop condition to generate correct Fibonacci series.

--- fibonacci_series.c
+++ fibonacci_series.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d",&n);
     int firstNum = 0,secondNum =1;
     printf("%d %d ",firstNum,secondNum);
-    for(int i=2;i<=n;i++) {
+    for(int i=2;i<n;i++) {
         int nextNum = firstNum + secondNum;
         printf("%d ",nextNum);
         firstNum = secondNum;
