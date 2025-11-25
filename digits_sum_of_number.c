ad32c3194eecec7135992c1f3dbbdbe044191920
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-25 08:47:17 +0530

    sum of digits of a number

--- digits_sum_of_number.c
+++ digits_sum_of_number.c
@@ -5,7 +5,7 @@ int main() {
     scanf("%d",&num);
     int originalNum = num;
     int sum = 0;
-    while(num>0) {
+    while(num!=0) {
         int digit =num %10;
         sum += digit;
         num /= 10;
