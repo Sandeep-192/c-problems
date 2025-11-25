ebb8ca36dc11566b31419d063a594349ac7d913b
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-25 07:45:40 +0530

    factorial of a number

---
+++ factorial_of_num.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main() {
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    int factorial =1;
+    for(int i=2;i<=num;i++) {
+        factorial *= i;
+    }
+    printf("Factorial of %d is %d\n",num,factorial);
+    return 0;
+}
