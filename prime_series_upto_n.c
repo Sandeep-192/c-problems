789171076868a6f778110e2513d5401e0a9445da
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-25 17:40:50 +0530

    prime number series up to nth number
    This program prompts the user for a number and prints all prime numbers up to that number.

---
+++ prime_series_upto_n.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter n for  upto #nth number:");
+    scanf("%d",&n);
+    for(int i=2;i<=n;i++) {
+        int isprime = 1;
+        for(int j=2;j*j<=i;j++) {
+            if(i%j == 0) {
+                isprime =0;
+                break;
+            }
+        }
+        if(isprime) {
+            printf("%d ",i);
+        }
+    }
+    return 0;
+}
