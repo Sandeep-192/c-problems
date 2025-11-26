6dda35ac16d467fa4f26479d53a7fbba54ef6bea
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-26 11:55:18 +0530

    left angle triangle pattern

---
+++ left_triangle_pattern.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main() {
+    int n;
+    printf("Enter n:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++) {
+        for(int j=1;j<=n-i;j++) { //spaces
+            printf(" ");
+        }
+        for(int j=1;j<=i;j++) { //stars
+            printf("*");
+        }
+
+        printf("\n");
+    }
+    return 0;
+}
