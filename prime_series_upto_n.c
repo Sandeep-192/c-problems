e2fa49a4f808df484983bd6d745da61dbbe8858e
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-26 07:15:19 +0530

    prime number series up to nth number

--- prime_series_upto_n.c
+++ prime_series_upto_n.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 int main() {
     int n;
-    printf("Enter n for  upto #nth number:");
+    printf("Enter n for prime numbers upto #n :");
     scanf("%d",&n);
+    if(n<2) {
+        printf("no prime numbers");
+        return 0;
+    }
     for(int i=2;i<=n;i++) {
         int isprime = 1;
         for(int j=2;j*j<=i;j++) {
             if(i%j == 0) {
-                isprime =0;
+                isprime = 0;
                 break;
-            }
+            } 
         }
         if(isprime) {
             printf("%d ",i);
