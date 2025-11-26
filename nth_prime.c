ff9ead754a86bc3a6b72dde14071d32a461e0c86
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-26 07:32:12 +0530

    nth prime number

--- nth_prime.c
+++ nth_prime.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 int main() {
     int n;
-    printf("Enter n for #nth number:");
+    printf("Enter n for prime numbers upto #n :");
     scanf("%d",&n);
-    int num = 2;
+    if(n<1) {
+        printf("no prime number");
+    }
     int count = 0;
+    int num = 2;
     while(count < n) {
         int isprime = 1;
         for(int i=2;i*i<=num;i++) {
-            if(num%i== 0) {
-                isprime =0;
+            if(num%i == 0) {
+                isprime = 0;
                 break;
             }
         }
@@ -17,7 +20,7 @@ int main() {
             count++;
         }
         if(count == n) {
-            printf("%dth prime number is : %d\n",n,num);
+            printf("%dth prime is %d\n",n,num);
             break;
         }
         num++;
