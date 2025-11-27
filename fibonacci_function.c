057f9ed7a17011f704b1ff2a1ae78b0f37d0e4b5
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-27 07:19:33 +0530

    fibonacci_function.c

--- fibonacci_function.c
+++ fibonacci_function.c
@@ -15,7 +15,7 @@ int fibonacci(int num) {
     if(num == 1) {
         return 0;
     }
-    if(num == 2 || num == 2) {
+    if(num == 2) {
         return 1;
     }
     return fibonacci(num-1)+fibonacci(num-2); 
