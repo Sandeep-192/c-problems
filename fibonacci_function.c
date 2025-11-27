10fe1f2061f0937888e80d4b7057ca2962916fb9
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-27 05:59:37 +0530

    find nth num in fibonacci series using function

---
+++ fibonacci_function.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int fibonacci(int num);
+int main() {
+    int num;
+    printf("Enter a number:");
+    scanf("%d",&num);
+    if(num <= 0) {
+        printf("No fibonacci number!");
+    } else {
+        printf("fibonacci(%d):%d",num,fibonacci(num));
+    }
+    return 0;
+}
+int fibonacci(int num) {
+    if(num == 1) {
+        return 0;
+    }
+    if(num == 2 || num == 2) {
+        return 1;
+    }
+    return fibonacci(num-1)+fibonacci(num-2); 
+}
