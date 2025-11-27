e9d52e981eda5878ba1d6b507dfd5123b925b8cc
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-27 06:14:43 +0530

    sum of 1 to n numbers using function

---
+++ one_to_n_sum_function.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int sum(int n);
+int main() {
+    int n;
+    printf("Enter a number:");
+    scanf("%d",&n);
+    if(n < 0) {
+        return 0;
+    }
+    printf("sum of numbers upto %d is = %d",n,sum(n));
+    return 0;
+}
+int sum(int n) {
+    if(n == 0) {
+        return 0;
+    }
+    return sum(n-1) + n;
+}
