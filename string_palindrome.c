deef89ccc6ea7184e90d11d62db6be2e5f92bb47
sandeepkolleti <sandeepkolleti1919@gmail.com>
2025-11-28 08:44:55 +0530

    check the string is palindrome or not

---
+++ string_palindrome.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+int main () {
+    char originalString[20];
+    printf("Enter string:");
+    scanf("%[^\n]",originalString);
+    char string[20];
+    strcpy(string, originalString);
+    int start = 0;
+    int end = strlen(string) - 1;
+    while(start < end) {
+        char temp = string[start];
+        string[start] = string[end];
+        string[end] = temp;
+        start++;
+        end--;
+    }
+    if(strcmp(originalString, string) == 0) {
+        printf("%s is a palindrome\n",originalString);
+    }else {
+        printf("%s is not a palindrome\n",originalString);
+    }
+    return 0;
+}
